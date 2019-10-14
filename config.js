8527852b765e9ed6b562a6b20c98c0fa8bb0ee34
generaltune <1499326562@qq.com>
2019-10-14 09:26:28 +0800

    修改网站配置信息

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 var CONFIG = {
   // your website's title
-  document_title: "ECMAScript 6入门",
+  document_title: "ITCASTZM 的技术博客",
 
   // index page
   index: "README.md",
@@ -9,7 +9,7 @@ var CONFIG = {
   sidebar_file: "sidebar.md",
 
   // where the docs are actually stored on github - so you can edit
-  base_url: "https://github.com/ruanyf/es6tutorial/edit/gh-pages",
+  base_url: "https://github.com/itcastzm/itcastzm.github.io/edit/master/",
 };
 
 // **************************
